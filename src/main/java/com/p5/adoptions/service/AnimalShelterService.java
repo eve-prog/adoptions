46bb26fa7adb7e8874ac6554214698f3fe42aba9
eve-prog <strugurel123->
2021-06-09 20:34:10 +0300

    Exception Handling

--- src/main/java/com/p5/adoptions/service/AnimalShelterService.java
+++ src/main/java/com/p5/adoptions/service/AnimalShelterService.java
@@ -5,20 +5,25 @@ import com.p5.adoptions.model.AnimalShelterDTO;
 import com.p5.adoptions.model.adapters.AnimalShelterAdapter;
 import com.p5.adoptions.model.validations.OnCreate;
 import com.p5.adoptions.model.validations.OnUpdate;
+import com.p5.adoptions.repository.animals.Animal;
 import com.p5.adoptions.repository.animals.AnimalRepository;
 import com.p5.adoptions.repository.shelter.AnimalShelter;
 import com.p5.adoptions.repository.shelter.AnimalShelterRepository;
+import com.p5.adoptions.service.exceptions.AnimalShelterNotFoundException;
+import com.p5.adoptions.service.exceptions.ShelterAddressException;
+import com.p5.adoptions.service.exceptions.Violation;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.logging.Logger;
 
 @Service
 @Validated
 public class AnimalShelterService {
-
     private final AnimalShelterRepository animalShelterRepository;
     private final AnimalRepository animalRepository;
 
@@ -42,22 +47,31 @@ public class AnimalShelterService {
     public AnimalShelterDTO updateShelter(@Valid AnimalShelterDTO shelterDTO) {
 
         validateShelter(shelterDTO);
-        animalShelterRepository.findById(shelterDTO.getId()).orElseThrow(() -> new RuntimeException("Shelter not found"));
+        // try catch block with exception propagation
+//        try {
+//            validateShelter(shelterDTO);
+//        } catch (RuntimeException ex) {
+//            Logger.getAnonymousLogger().warning(ex.getMessage());
+//            throw new RuntimeException(ex);
+//        }
 
         return AnimalShelterAdapter.toDto(animalShelterRepository.save(AnimalShelterAdapter.fromDto(shelterDTO)));
     }
 
     private void validateShelter(AnimalShelterDTO shelterDTO) {
-        if (!shelterDTO.getAddress().toLowerCase(Locale.ROOT).contains("brasov")) {
-            throw new RuntimeException("Shelter is not from BRASOV");
+
+        if(!shelterDTO.getAddress().toLowerCase(Locale.ROOT).contains("brasov")) {
+            throw new ShelterAddressException(new Violation("address", "Shelter is not from BRASOV", shelterDTO.getAddress()));
         }
+
         for (AnimalDTO animal : shelterDTO.getAnimals()) {
             if (!animal.getPhotoUrl().toLowerCase(Locale.ROOT).contains("https")){
-                throw new RuntimeException("Animal does not have valid url");
+                throw new RuntimeException("Animal does not have a valid url");
             }
         }
 
-        animalShelterRepository.findById(shelterDTO.getId()).orElseThrow(() -> new RuntimeException("Shelter not found"));
+        animalShelterRepository.findById(shelterDTO.getId())
+                .orElseThrow(() -> new AnimalShelterNotFoundException("Shelter not found"));
     }
 
     public List<AnimalShelterDTO> getAll() {
