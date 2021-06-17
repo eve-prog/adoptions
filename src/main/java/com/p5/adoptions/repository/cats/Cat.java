1cb05e06ec8e67009d496b8514d290129f75bfb7
eve-prog <strugurel123->
2021-06-17 17:15:30 +0300

    Security

--- src/main/java/com/p5/adoptions/repository/cats/Cat.java
+++ src/main/java/com/p5/adoptions/repository/cats/Cat.java
@@ -8,7 +8,6 @@ public class Cat extends Animal
 {
     private String purrLevel;
 
-
     public Cat()
     {
     }
---
+++ src/main/java/com/p5/adoptions/repository/users/User.java
@@ -0,0 +1,41 @@
+package com.p5.adoptions.repository.users;
+
+import javax.persistence.*;
+
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Integer id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public User setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+}
